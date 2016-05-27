bca83b3687d1663927446c10f4a34c54cf80645c
Wirewords-PC <sarathm00@gmail.com>
2016-05-27 13:19:39 +0530

    Simplified code, Scaling inputs

--- Code/adc.c
+++ Code/adc.c
@@ -21,9 +21,13 @@
 #pragma config CP = OFF         // Flash Program Memory Code Protection bit (Code protection off)
 
 #define _XTAL_FREQ 20000000
+#define SLR_RATIO 0.247
+#define BATT_RATIO 0.3103
+#define LDR_RATIO 1.0
 #include "LCD.h"
 
-void display_float(float val);
+void display_float(const char *label, float val, int pos);
+void lcd_pos_select(int pos);
 void adc_init()
 {
     TRISA = 0xff;         // AN0
@@ -61,40 +65,63 @@ float convertToVolt(int adc_value)
     return res;
 }
 
-void display_float(float val)
+void display_float(const char *label, float val, int pos)
 {
     char * buf;
     int status;
     buf = ftoa(val, &status);
     buf[5] = '\0';                  //Float precision Adjustment
+    lcd_pos_select(pos);
+    WriteStringToLCD(label);
     WriteStringToLCD(buf);
+}
+
+float read_channel(int channel, float ratio, const char *label, int pos)
+{
+    float value;
+    int adc_value;
+    
+    adc_value = read_adc(channel);
+    value = convertToVolt(adc_value);
+    value = value/ratio;
+    display_float(label,value, pos);
+    return value;
+}
+
+void lcd_pos_select(int pos)
+{
+    switch(pos)
+    {
+        case 0: 
+            WriteCommandToLCD(0x80);
+            break;
+        
+        case 1: 
+            WriteCommandToLCD(0x89);
+            break;
+            
+        case 2: 
+            WriteCommandToLCD(0xC0);
+            break;
+    }
+}
+
+void read_inputs(float *slr, float *battery, float *ldr)
+{
+    *slr = read_channel(0, SLR_RATIO, "S:", 0);
+        
+    *battery = read_channel(1, BATT_RATIO, "B:", 1);
+        
+    *ldr = read_channel(2, LDR_RATIO, "L:", 2);   
 }
 
 void main()
 {
-    int adc_value;
-    float an0, an1, an2;
+    float slr, battery, ldr;
     adc_init();
     InitLCD();
-    TRISB = 0x00;
     while(1)
     {
-        WriteCommandToLCD(0x80);
-        adc_value = read_adc(0);
-        an0 = convertToVolt(adc_value);
-        WriteStringToLCD("S:");
-        display_float(an0);
-        
-        WriteCommandToLCD(0x89);
-        adc_value = read_adc(1);
-        an1 = convertToVolt(adc_value);
-        WriteStringToLCD("B:");
-        display_float(an1);
-        
-        WriteCommandToLCD(0xC0);
-        adc_value = read_adc(2);
-        an2 = convertToVolt(adc_value);
-        WriteStringToLCD("L:");
-        display_float(an2);
+        read_inputs(&slr, &battery, &ldr);
     }
 }
